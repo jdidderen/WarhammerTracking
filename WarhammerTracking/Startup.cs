530b48a7ea25722c16d2d6a81eb9fdc8cff790a9
Didderen Jérémy <didderen.jeremy@jdi.me>
2019-10-15 18:46:33 +0200

    #9 Localization

--- WarhammerTracking/Startup.cs
+++ WarhammerTracking/Startup.cs
@@ -32,8 +32,6 @@ namespace WarhammerTracking
         
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -48,33 +46,16 @@ namespace WarhammerTracking
             services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            
-            // Translations
-//            
-//            var supportedCultures = new List<CultureInfo>{
-//                new CultureInfo("en-US"),
-//                new CultureInfo("fr-FR")
-//            };
-//            services.Configure<RequestLocalizationOptions>(options =>
-//            {
-//                options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
-//                options.SupportedUICultures = supportedCultures;
-//                options.SupportedUICultures = supportedCultures;
-//            });
-//            
-//            services.AddLocalization(options => options.ResourcesPath = "Translations");
-            
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
             services.AddScoped<ArmyDataAccessProvider>();
             services.AddScoped<GameDataAccessProvider>();
             services.AddScoped<GameRequestDataAccessProvider>();
             services.AddScoped<UserDataAccessProvider>();
-            services.AddScoped<AppState, AppState>();
+            services.AddScoped<AppState>();
             
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,UserManager<ApplicationUser> userManager,IServiceProvider serviceProvider)
         {
             UpdateDatabase(app);
@@ -86,7 +67,6 @@ namespace WarhammerTracking
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             
@@ -95,7 +75,19 @@ namespace WarhammerTracking
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
-            app.UseRequestLocalization();
+            var supportedCultures = new[]
+            {
+                new CultureInfo("en"),
+                new CultureInfo("fr"),
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -133,23 +125,17 @@ namespace WarhammerTracking
         {
             var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-
             IdentityResult roleResult;
-            //Adding Admin Role
             var AdminRoleCheck = await RoleManager.RoleExistsAsync("Admin");
             if (!AdminRoleCheck)
             {
-                //create the roles and seed them to the database
                 roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
             }
             var UserRoleCheck = await RoleManager.RoleExistsAsync("User");
             if (!UserRoleCheck)
             {
-                //create the roles and seed them to the database
                 roleResult = await RoleManager.CreateAsync(new IdentityRole("User"));
             }
-            //Assign Admin role to the main User here we have given our newly registered 
-            //login id for Admin management
             ApplicationUser user = await UserManager.FindByEmailAsync("didderen.jeremy@jdi.me");
             await UserManager.AddToRoleAsync(user, "Admin");
         }
